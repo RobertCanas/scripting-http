8894d3495830285a6eb61bc3f7c65cfaf8b7527d
Robert Canas <robert_canas@hotmail.com>
2017-02-13 21:21:28 +0000

    Callback revision and Step 5 implementaiton

--- step5-module/http-functions.js
+++ step5-module/http-functions.js
@@ -1,34 +1,19 @@
 var https = require('https');
-
-function getHTML(options, callback) {
+module.exports = function getHTML(options, callback) {
 
   /* Add your code here */
   https.get(options, function(response) {
-    //var bufferData;
     var bufferDataArray = [];
     response.setEncoding('utf8');
 
     response.on('data', function printHTML(html) {
-      //bufferData += data;
       bufferDataArray.push(html);
     });
 
     response.on('end', function() {
-      //console.log(bufferData);
       callback(bufferDataArray.toString());
       console.log('Response stream complete.');
     });
   });
 }
 
-function printHTML(html) {
-  console.log(html);
-}
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step4.html'
-};
-
-printHTML();
-getHTML(requestOptions, printHTML);
