578e1f013dbfd8e7bfb0444ae73283ba9df7b169
Robert Canas <robert_canas@hotmail.com>
2017-02-13 20:20:35 +0000

    Step 2 implementation

--- printing-with-buffered-strings.js
+++ printing-with-buffered-strings.js
@@ -1,27 +1,28 @@
-function getAndPrintHTMLChunks() {
-
+function getAndPrintHTML() {
   var https = require('https');
-
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   /* Add your code here */
   https.get(requestOptions, function(response) {
-
+    //var bufferData;
+    var bufferDataArray = [];
     response.setEncoding('utf8');
 
     response.on('data', function(data) {
-      console.log(data + '\n');
+      //bufferData += data;
+      bufferDataArray.push(data);
     });
 
     response.on('end', function() {
+      //console.log(bufferData);
+      console.log(bufferDataArray.toString());
       console.log('Response stream complete.');
     });
 
   });
-
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
